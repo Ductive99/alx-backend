b76ba89ff60422778b4cae533b5ef6634921a880
El Houssain Souhail <sou.ductive@gmail.com>
2024-05-19 22:24:25 +0100

    Added documentation + Fixed annotation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -14,7 +14,8 @@ Task 0:
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returs start index and end index"""
     start_idx = (page - 1) * page_size
     end_idx = page * page_size
     return (start_idx, end_idx)
