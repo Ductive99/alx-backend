c96b052529899df9ebb63b808a3f6e49a6217133
El Houssain Souhail <sou.ductive@gmail.com>
2024-05-23 00:40:52 +0100

    Added exec rights

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""Task 1
+"""Last-In First-Out caching module.
 """
 from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """First-In First-Out Cache Class
+class LIFOCache(BaseCaching):
+    """Last-In First-Out Cache Class
     """
     def __init__(self):
         """Initializes the cache
@@ -19,10 +20,12 @@ class FIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(False)
-            print("DISCARD:", first_key)
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """Returns the item based on the key
